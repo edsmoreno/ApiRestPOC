9b0dedfa96ba3c124da89f426fa7f1d5c0576ad0
edsmoreno <edsmoreno@gmail.com>
2021-10-31 23:54:47 -0300

    Criando Domain com Cliente, adicionando referencias EntityFramework e mapeando Domain como DbSet

---
+++ ApiRest.Data/ContextoPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using ApiRest.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ApiRest.Data
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ ApiRest.Domain/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        [Required(ErrorMessage = "o campo {0} é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres!")]
+        public Guid id { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public int idade { get; set; }
+    }
+}
